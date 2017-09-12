a4d183e2e435af09d2c73ae4399ce85a246ca394
devravim <dev.ravim@gmail.com>
2017-09-13 00:17:38 +0530

    muliplication

--- src/com/rv/Test1.java
+++ src/com/rv/Test1.java
@@ -13,6 +13,8 @@ public class Test1 {
 		
 		System.out.println(logic.addTwoNumbers(20, 58));
 		
+		System.out.println(logic.multiplyTwoNumbers(20, 58));
+		
 
 	}
 
--- src/com/rv/demo1/Logics.java
+++ src/com/rv/demo1/Logics.java
@@ -5,4 +5,8 @@ public class Logics {
 	public int addTwoNumbers(int num1,int num2) {
 		return num1+num2;
 	}
+	
+	public int multiplyTwoNumbers(int num1,int num2) {
+		return num1*num2;
+	}
 }
