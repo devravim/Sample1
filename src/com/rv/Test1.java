0ccc6021d82ea4452cef7141b769a23ebcfc8e56
devravim <dev.ravim@gmail.com>
2017-09-12 22:13:00 +0530

    initial commit

---
+++ src/com/rv/Test1.java
@@ -0,0 +1,15 @@
+package com.rv;
+
+import com.rv.demo1.Logics;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		Logics logic=new Logics();
+		
+		System.out.println(logic.addTwoNumbers(20, 25));
+		
+
+	}
+
+}
---
+++ src/com/rv/demo1/Logics.java
@@ -0,0 +1,8 @@
+package com.rv.demo1;
+
+public class Logics {
+
+	public int addTwoNumbers(int num1,int num2) {
+		return num1+num2;
+	}
+}
